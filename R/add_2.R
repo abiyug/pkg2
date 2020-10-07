e48466e3b9b530f75d1032feabaaf1571595c3d4
abiyug <abiyu@antexusolutions.com>
2020-10-07 12:40:05 -0400

    add function add_2

---
+++ R/add_2.R
@@ -0,0 +1,14 @@
+#' @title Add plus 2 to a number (or #' Add plus 2 to a number, roxygen2 will guess it's the title)
+#'
+#' @description More information about the function but still short.
+#'
+#' @param x A number (integer)
+#' @return a number object
+#' @export
+#'
+#' @examples
+#' add_2(2)
+
+add_2 <- function(x) {
+                      x + 2
+                     }
